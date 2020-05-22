199d8736bf890ecfebff1a6019586d8e2ce76ed4
Edward Barber <=>
2020-05-22 17:28:37 -0400

    My solution to the Two Sum excercise

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+
+// Two Sum
+
+let practiceArray = [1, 4, 10, 16, 7, 3, 3]
+let target = 14
+
+func twoSumProblem(_ nums: [Int], target: Int) -> [Int]? {
+    
+    var startIndex = 0
+    var endIndex = nums.count - 1
+    
+    let sortedNums = nums.sorted()
+    
+    while startIndex < endIndex {
+        
+        let sum = sortedNums[startIndex] + sortedNums[endIndex]
+        
+        if sum == target {
+            return [sortedNums[startIndex], sortedNums[endIndex]]
+        } else if sum < target {
+            startIndex += 1
+        } else {
+            endIndex -= 1
+        }
+    }
+    
+    return nil
+}
+
+twoSumProblem(practiceArray, target: target)
